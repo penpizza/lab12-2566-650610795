2f68cdfc77152385eb140204c4365fdd484515c6
penpizza <penpicha.thongkham@gmail.com>
2023-08-03 15:14:25 +0700

    completed

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,13 +1,17 @@
 import { ThemeContext } from "@/contexts/ThemeContext";
 import { useContext } from "react";
-
+import { LangContext } from "@/contexts/LangContext";
 export const Detail = () => {
   const { theme } = useContext(ThemeContext);
+  const { lang } = useContext(LangContext);
+
+  const sth = () =>{
+      if(theme.name === "dark") return lang.detail.darkActivated;
+      else return lang.detail.lightActivated;
+  }
   return (
     <p className="text-center" style={{ color: theme.fgColor }}>
-      Display activated theme message here (EN or TH)
-      {/* tip: Get information from "theme" variable to determine 
-      which theme is currently selected */}
-    </p>
+      {sth()}
+      </p>
   );
 };
